8c2b468a46a91cdfe0db1a944308cea943d2d43a
Artem Lisovik <107492333+ArtemLisovik@users.noreply.github.com>
2023-08-06 16:44:55 +0300

    final v1.2

--- src/containers/Promo/Promo.tsx
+++ src/containers/Promo/Promo.tsx
@@ -13,17 +13,20 @@ export const Promo = () => {
 
 
                     <h1 className="info__title">
-                        <span className="info__title-text">Artem Lisovik</span>
+                        <span className="info__title-text">AL 
+                            <span className='title__sub'>studio</span>.
+                        </span>
                         <span className="info__title-wrapper"></span>
                     </h1>
                     <h2 className="info__subtitle">
-                        <span className="info__subtitle-text">Студия веб-разработки</span>
+                        <span className="info__subtitle-text">Студия веб-разработки <br /></span>
                         <span className="info__subtitle-wrapper"></span>
                     </h2>
                     <div className="info__subtitle-price">
-                        <span className="info__subtitle-price-text">Создание сайтов любой сложности</span>
-                        <span className="info__subtitle-wrapper"></span>
-                        {/* <Link
+                        {/* <span className="info__subtitle-price-text">Сотрудничайте только с профессионалами</span>
+                        <span className="info__subtitle-wrapper"></span> */}
+
+                        <Link
                             to="contactUs"
                             spy={true}
                             smooth={true}
@@ -31,21 +34,21 @@ export const Promo = () => {
                             duration={0}
                             className="button__main">
                             Заказать сайт
-                        </Link> */}
+                        </Link>
                     </div>
-                    
+
                     <Link
-                    to="portfolio"
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration={0}
-                    className="info__scroll-link">
-                      <span className="info__mouse">
+                        to="portfolio"
+                        spy={true}
+                        smooth={true}
+                        offset={0}
+                        duration={0}
+                        className="info__scroll-link">
+                        <span className="info__mouse">
                             <span className="info__mouse-scroll"></span>
                         </span>
                         <span className="info__mouse-text">Scroll</span>
-                </Link>
+                    </Link>
                 </div>
             </div>
         </section>
